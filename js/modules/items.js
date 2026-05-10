e4b3cfa41a7947253fc549bb12c98e4fa5fc2aac
Rifqi Khairil <Nadya0082@stmik-wp.ac.id>
2026-05-10 13:46:48 +0700

    YYYY MM DD HH MM

--- js/modules/items.js
+++ js/modules/items.js
@@ -2,55 +2,55 @@ const items = [
     {
         name: "Donut J.CO",
         price: 15000,
-        desc: "Donut Bermacam Rasa dan Variasi",
+        desc: "⭐ 4.9 (40+)",
         image: "assets/images/donut.jpg"
     },
     {
         name: "Risoles Mayonaise",
         price: 2000,
-        desc: "Risoles Berisi Sosis dan Mayonaise",
+        desc: "⭐ 4.8 (30+)",
         image: "assets/images/risolmayo.jpg"
     },
     {
         name: "Wonton",
         price: 5000,
-        desc: "Wonton Pedas Dengan Chilli Oil",
+        desc: "⭐ 4.7 (25+)",
         image: "assets/images/wonton.jpg",
     },
     {
         name: "Ceker Mercon",
         price: 6000,
-        desc: "Ceker itu kaki ayam klo gk salah",
+        desc: "⭐ 4.6 (20+)",
         image: "assets/images/cekermercon.jpg"
     },
     {
         name: "Nasi Goreng",
         price: 12000,
-        desc: "Nasi Goreng dengan Telur",
+        desc: "⭐ 4.5 (15+)",
         image: "assets/images/donut.jpg"
     },
     {
         name: "Mie Ayam",
         price: 10000,
-        desc: "Mie Ayam Gurih",
+        desc: "⭐ 4.4 (10+)",
         image: "assets/images/mieayam.jpg"
     },
     {
         name: "Es Cokelat",
         price: 8000,
-        desc: "Es Cokelat Manis",
+        desc: "⭐ 4.3 (5+)",
         image: "assets/images/coklat.jpg"
     },
     {
         name: "Es Doger",
         price: 8000,
-        desc: "Es Doger Manis & Segar",
-        image: "assets/images/esdoger1.jpg"
+        desc: "⭐ 4.2 (5+)",
+        image: "assets/images/esdoger2.jpg"
     },
     {
         name: "Butterschoth Coffee",
         price: 12000,
-        desc: " rasa manis creamy dengan sentuhan karamel buttery yang lembut dan hangat",
+        desc: "⭐ 4.1 (5+)",
         image: "assets/images/butterschoth.jpg"
     }
     // {
